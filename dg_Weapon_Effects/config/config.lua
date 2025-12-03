16dd7a720e2e354877c99a77fa509ae5f907cf80
DGDIPPER <109880991+DGDIPPER@users.noreply.github.com>
2025-12-03 20:02:13 +0000

    Add files via upload

--- dg_Weapon_Effects/config/config.lua
+++ dg_Weapon_Effects/config/config.lua
@@ -15,13 +15,9 @@ Config.AllEffects = {
 -- effect: single string or {multiple, effects}
 -- duration: seconds
 Config.Weapons = {
-    [`WEAPON_YTASER`]      = {slowdown = 0.5,  effect = {"Drunk", "RedMist"}, duration = 25},
-    --[`WEAPON_PISTOL`]      = {slowdown = 0.5,  effect = {"Drunk", "RedMist"}, duration = 20},
-    --[`WEAPON_PUMPSHOTGUN`]  = {slowdown = 0.2,  effect = {"ExplosionJosh3", "RedMist", "PPRed"}, duration = 6},
-    --[`WEAPON_MICROSMG`]     = {slowdown = 0.5,  effect = {"PPBlue", "MP_Bull_tost"}, duration = 4},
-    --[`WEAPON_ASSAULTRIFLE`] = {slowdown = 0.3,  effect = {"PPRed", "MP_Celeb_Lose"}, duration = 6},
-    --[`WEAPON_SNIPERRIFLE`]  = {slowdown = 0.25, effect = {"FocusIn", "ChopVision"}, duration = 7},
-    --[`WEAPON_GRENADE`]      = {slowdown = 0.1,  effect = {"MP_race_crash", "DeathFailOut"}, duration = 8},
-    --[`WEAPON_MOLOTOV`]      = {slowdown = 0.2,  effect = {"MP_Bull_tost", "PPOrange"}, duration = 9},
-    --[`WEAPON_STUNGUN`]      = {slowdown = 0.5,  effect = {"Drunk", "ChopVision"}, duration = 30}
+    [`WEAPON_PISTOL`]      = {slowdown = 0.5,  effect = {"Drunk", "RedMist"}, duration = 20},
+    [`WEAPON_PUMPSHOTGUN`]  = {slowdown = 0.2,  effect = {"ExplosionJosh3", "RedMist", "PPRed"}, duration = 6},
+    [`WEAPON_MICROSMG`]     = {slowdown = 0.5,  effect = {"PPBlue", "MP_Bull_tost"}, duration = 4},
+    [`WEAPON_ASSAULTRIFLE`] = {slowdown = 0.3,  effect = {"PPRed", "MP_Celeb_Lose"}, duration = 6},
+    [`WEAPON_STUNGUN`]      = {slowdown = 0.5,  effect = {"RedMist", "Drunk"}, duration = 20}
 }
